b8718f164b5696e27d80f221f701fe1b281053df
Kevin Kellar <kellar.kevin@gmail.com>
2019-02-13 11:08:49 -0800

    robust nonblock p control now takes a struct of args

--- FOLD/control.h
+++ FOLD/control.h
@@ -6,15 +6,23 @@
 #include "wiring.h"
 #include "wheel.h"
 
-void p_control_non_block(
-    int16_t* result_speed,
-    uint16_t* end_condition_count,
-    uint8_t pin_ultrasonic,
-    int16_t mm_target,
-    int16_t mm_cutoff,
-    float pk,
-    int16_t max_speed,
-    uint8_t pin_switch,
-    uint16_t max_end_condition_count);
+struct p_control_result
+{
+	int16_t result_speed;
+	uint16_t end_condition_count;
+};
+
+struct p_control_args
+{
+	uint8_t pin_ultrasonic;
+    int16_t mm_target;
+    int16_t mm_cutoff;
+    float pk;
+    int16_t max_speed;
+    uint8_t pin_switch;
+    uint16_t max_end_condition_count;
+};
+
+void p_control_non_block(struct p_control_result* result, struct p_control_args* args);
 
 #endif
--- FOLD/control.ino
+++ FOLD/control.ino
@@ -8,49 +8,41 @@ float signum(float f)
     return 1;
 }
 
-void p_control_non_block(
-    int16_t* result_speed,
-    uint16_t* end_condition_count,
-    uint8_t pin_ultrasonic,
-    int16_t mm_target,
-    int16_t mm_cutoff,
-    float pk,
-    int16_t max_speed,
-    uint8_t pin_switch,
-    uint16_t max_end_condition_count)
+void p_control_non_block(struct p_control_result* result, struct p_control_args* args)
 {
+	uint16_t abs_max_speed;
     float cm;
     float val;
 
-    max_speed = abs(max_speed);
+    abs_max_speed = ((uint16_t) abs(args->max_speed));
     
-    cm = echo_test_mm(pin_ultrasonic);
+    cm = echo_test_mm(args->pin_ultrasonic);
     val = cm;
-    val -= mm_target;
-    val *= pk;
+    val -= args->mm_target;
+    val *= args->pk;
 
-    if(abs(val) > max_speed)
-        val = max_speed * signum(val);
+    if(abs(val) > abs_max_speed)
+        val = abs_max_speed * signum(val);
     if (abs(val) < 10)
         val = 0;
     else if (val < 80)
         val = 80;
 
-    *result_speed = (int16_t) val;
+    result->result_speed = (int16_t) val;
 
-    if(mm_cutoff > 0)
+    if(args->mm_cutoff > 0)
     {
-        if(cm < mm_cutoff)
-            *end_condition_count -= 1;
+        if(cm < args->mm_cutoff)
+            result->end_condition_count -= 1;
         else
-            *end_condition_count = *end_condition_count+1 > max_end_condition_count ? max_end_condition_count : *end_condition_count + 1;
+            result->end_condition_count = result->end_condition_count+1 > args->max_end_condition_count ? args->max_end_condition_count : result->end_condition_count + 1;
     }
-    if(mm_cutoff < 0 && cm > -mm_cutoff)
+    if(args->mm_cutoff < 0 && cm > -args->mm_cutoff)
     {
-        if(cm > -mm_cutoff)
-            *end_condition_count -= 1;
+        if(cm > -args->mm_cutoff)
+            result->end_condition_count -= 1;
         else
-            *end_condition_count = *end_condition_count+1 > max_end_condition_count ? max_end_condition_count : *end_condition_count + 1;
+            result->end_condition_count = result->end_condition_count+1 > args->max_end_condition_count ? args->max_end_condition_count : result->end_condition_count + 1;
     }
 
     //300 --> 30cm
